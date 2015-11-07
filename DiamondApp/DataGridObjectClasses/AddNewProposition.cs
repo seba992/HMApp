cf78943fffc6b40607a1eea13189b73595a7dbe9
Sebastian Nalepka <seba992@gmail.com>
2015-11-07 16:19:01 +0100

    Auto update Proposition valid date Update adminviewwindow

--- DiamondApp/DataGridObjectClasses/AddNewProposition.cs
+++ DiamondApp/DataGridObjectClasses/AddNewProposition.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DiamondApp.Tools;
 
 namespace DiamondApp.DataGridObjectClasses
 {
-    public class AddNewProposition
+    public class AddNewProposition : ObservableObject
     {
+        private DateTime _updateDate;
+        private DateTime _validityPropDate;
+
         public string CompanyName
         {
             get { return "Zmienić tą durną nazwę " + Environment.NewLine + " nic innego nie przyszło mi do głowy "+Environment.NewLine+" :("; }
@@ -26,7 +30,26 @@ namespace DiamondApp.DataGridObjectClasses
             set { }
         }
 
-        public DateTime UpdateDate { get; set; }
+        public DateTime UpdateDate
+        {
+            get { return _updateDate; }
+            set
+            {
+                _updateDate = value;
+                ValidityPropDate = value.AddDays(4);
+            }
+        }
+
+        public DateTime ValidityPropDate
+        {
+            get { return _validityPropDate; }
+            set
+            {
+                _validityPropDate = value;
+                RaisePropertyChanged("ValidityPropDate");
+            }
+        }
+
         public string UserFirstName { get; set; }
         public string UserSurname { get; set; }
         public string UserPhoneNum { get; set; }
