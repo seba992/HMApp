3f7c605ffef5c5146e53f9d5eb3a4b1e05d0759d
mateusz.ogiermann <mateusz.ogiermann@sap.com>
2015-11-11 23:51:49 +0100

    Change UserListView to DataGridView, trigger automatically update when edit rows

--- DiamondApp/Views/AdminMainView.xaml.cs
+++ DiamondApp/Views/AdminMainView.xaml.cs
@@ -2,7 +2,13 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Markup;
+using System.Linq;
 using DiamondApp.ViewModels;
+using System.Data;
+using DiamondApp.EntityModel;
+using System;
+using DiamondApp.DataGridObjectClasses;
+using System.Collections;
 
 namespace DiamondApp.Views
 {
@@ -72,6 +78,52 @@ namespace DiamondApp.Views
 
         private void SRElementySali2_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+
+        }
+
+        private void UserList_RowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
+        {
+            try
+            {
+                // Approach: When user finish changes values of cells in row, trigger update
+                // Get database Id of user from selected row in datagrid
+                object item = UserList.SelectedItem;
+                string ID = (UserList.SelectedCells[0].Column.GetCellContent(item) as TextBlock).Text;
+                //MessageBox.Show(ID);
+
+                //convert ID to int
+                int selected = Int32.Parse(ID);
+
+                //object which stores all deta from selected row
+                dynamic userRow = UserList.SelectedItem;
+
+                //MessageBox.Show(userRow.UserName);
+                //MessageBox.Show(userRow.UserEmail);
+
+                DiamondDBEntities _ctx = new DiamondDBEntities();
+                Users userUpdate = (from user in _ctx.Users
+                                    where user.Id == selected // int selected!!! you know what i want up to date
+                                    select user).First();
+
+                // update values in database by select from 'dynamic userRow' properties 
+                // UserName UserEmail etc. are in xaml in line:
+                // <TextBox Text="{Binding UserLogin, UpdateSourceTrigger=PropertyChanged}"/>
+                // need to update 'dynamic userRow' with Trigger, when user write text into cell
+
+                userUpdate.Name = userRow.UserName;
+                userUpdate.Surname = userRow.UserSurname;
+                userUpdate.PhoneNum = userRow.UserPhoneNumber;
+                userUpdate.Email = userRow.UserEmail;
+                userUpdate.Position = userRow.UserPosition;
+                userUpdate.AccountType = userRow.UserAccountType;
+                userUpdate.Login = userRow.UserLogin;
+
+                _ctx.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
 
         }
     }
