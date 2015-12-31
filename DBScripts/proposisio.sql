db0515b7c2a08db1604120369b50462f92f0c904
Łukasz Lis <lukasz.lis21@gmail.com>
2015-12-31 08:28:16 +0100

    poprawiony skrypt bazy danych i propozycja cenowa

--- DBScripts/proposisio.sql
+++ DBScripts/proposisio.sql
@@ -4,66 +4,65 @@ values (1,'a'),(2,'s');
 
 insert into Users (Name,Surname,PhoneNum, Email,Position,AccountType,Login,Password,FirstLogin)
 values 
-('Lidia' , 'Torzewska',	'512971553',	'ltorzewska@hotelediament.pl'	,'Regionalny Kierownik Sprzedaży',2, 'lito','test','t'),
-('Agata', 'Kleszko',	'506102076',	'akleszko@hotelediament.pl','Specjalista ds. sprzedaży',2,'agkl','test','t'),
-('Daria', 'Drewniok',	'506100952',	'ddrewniok@hotelediament.pl',	'Specjalista ds. sprzedaży',2,'dadr','test','t'),
-('Paweł','Jaszczyk',	'512007210','pjaszczyk@hotelediament.pl',	'Specjalista ds. sprzedaży',2 , 'paja','test','t'),
-( 'Michał',' Witkowicz',	'609060268',	'mwitkowicz@hotelediament.pl',	'Specjalista ds. sprzedaży',1,'miwi','test','t'),
-('Tomasz', 'Antolak',	'603051705',	'tantolak@hotelediament.pl',	'Specjalista ds. sprzedaży',2,'toan','test','t'),
-('Małgorzata', 'Michalak'	,'695219621',	'mmichalak@hotelediament.pl',	'Contracting Manager',2,'mami','test','t'),
-('Tomasz', 'Sikora',	'512971529',	'tsikora@hotelediament.pl',	'Specjalista ds. Sprzedaży',2,'tosi','test','t'),
-('Anna', 'Rosińska',	NULl,	'arosinska@hotelediament.pl',	'Specjalista ds. Sprzedaży',2,'anro','test','t'),
-('Maria' ,'Bonk',	'506101535',	'mbonk@hotelediament.pl',	'Specjalista ds. Sprzedaży',2,'mabo','test','t'),
-('Sylwia', 'Śliwka', '506101958',	'ssliwka@hotelediament.pl',	'Specjalista ds. Klienta wypoczynkowego',2,'sysl','test','t'),
-('Joanna', 'Gwiazdowska',	'605882838',	'jgwiazdowska@hotelediament.pl','Specjalista ds. Sprzedaży MICE',2,'jogw','test','t'),
-('Monika', 'Medwid',	'512971368',	'mmedwid@hotelediament.pl',	'Specjalista ds. Sprzedaży',2,'mome','test','t' ),
-('Marcin', 'Budniok', 	'667671471', 	'marcin.budniok@stylehotels.pl',	'Kierownik Działu Sprzedaży',2,'mabu','test','t'),
-('Daria', 'Drewniok',	'506100952',	'ddrewniok@hotelediament.pl',	'Specjalista ds. sprzedaży',2,'dadr','test','t'),
-('Justyna', 'Majchrzak',	'512971568','jmajchrzak@hotelediament.pl',	'Specjalista ds. sprzedaży',2,'juma','test','t'),
-('Magdalena', 'Jędruszek', 	'723550657',	'mjedruszek@hotelediament.pl',	'Specjalista ds. sprzedaży',2,'maje','test','t'),
-('Magdalena', 'Radzikowska-Buchała',	'506101894',	'mradzikowska@hotelediament.pl',	'Specjalista ds. Sprzedaży',2,'mara','test' ,'t'),
-('Magdalena', 'Szopa',	'512971427',	'mszopa@hotelediament.pl',	'Specjalista ds. Sprzedaży',2,'ma','sz' ,'t')
+('Tomasz' , 'Karolak',	'595584695',	'tkarol234@gmail.com'	,'Menadżer',1, 'toko','9b971780f63d4d90eb7475ed1e365ba78559f7b2fd557f1d2650f92e10d7f8e1','t'),
+('Agata', 'Przybysz',	'958612357',	'agataprzybysz@gmail.com','Specjalista ds. sprzedaży',2,'agpr','9b971780f63d4d90eb7475ed1e365ba78559f7b2fd557f1d2650f92e10d7f8e1','t'),
+('Daria', 'Warcab',		'951753654',	'warcabdaria@gmail.com',	'Specjalista ds. sprzedaży',2,'dawa','9b971780f63d4d90eb7475ed1e365ba78559f7b2fd557f1d2650f92e10d7f8e1','t'),
+('Paweł','Lis',			'564521654',	'plis76@wp.pl',	'Specjalista ds. sprzedaży',2 , 'pali','9b971780f63d4d90eb7475ed1e365ba78559f7b2fd557f1d2650f92e10d7f8e1','t'),
+('Michał','Komada',		'756489214',	'mkomada@wp.pl',	'Specjalista ds. sprzedaży',1,'miko','9b971780f63d4d90eb7475ed1e365ba78559f7b2fd557f1d2650f92e10d7f8e1','t'),
+('Tomasz', 'Wiglusz',	'654812384',	'tantolak@wp.pl',	'Specjalista ds. sprzedaży',2,'towi','9b971780f63d4d90eb7475ed1e365ba78559f7b2fd557f1d2650f92e10d7f8e1','t'),
+('Małgorzata', 'Rembisz','698546458',	'mmichalak@onet.pl',	'Dyrektor',1,'mare','9b971780f63d4d90eb7475ed1e365ba78559f7b2fd557f1d2650f92e10d7f8e1','t'),
+('Tomasz', 'Skowronek',	'475856185',	'tsikora@onet.pl',	'Specjalista ds. Sprzedaży',2,'tosk','9b971780f63d4d90eb7475ed1e365ba78559f7b2fd557f1d2650f92e10d7f8e1','t'),
+('Anna', 'Rosińska',		NULL,		'arosinska@onet.pl',	'Specjalista ds. Sprzedaży',2,'anro','9b971780f63d4d90eb7475ed1e365ba78559f7b2fd557f1d2650f92e10d7f8e1','t'),
+('Maria' ,'Krzemień',	'846484184',	'm_krzemień@interia.pl',	'Specjalista ds. Sprzedaży',2,'makr','9b971780f63d4d90eb7475ed1e365ba78559f7b2fd557f1d2650f92e10d7f8e1','t'),
+('Sylwia', 'Gruszka', 	'557487748',	'sgruszka785@interia.pl',	'Specjalista ds. Sprzedaży',2,'sygr','9b971780f63d4d90eb7475ed1e365ba78559f7b2fd557f1d2650f92e10d7f8e1','t'),
+('Joanna', 'Ulman',		'456861564',	'joanna_ulman@interia.pl','Specjalista ds. Sprzedaży',2,'joul','9b971780f63d4d90eb7475ed1e365ba78559f7b2fd557f1d2650f92e10d7f8e1','t'),
+('Monika', 'Duda',		'846843182',	'mduda@gmail.pl',	'Specjalista ds. Sprzedaży',2,'modu','9b971780f63d4d90eb7475ed1e365ba78559f7b2fd557f1d2650f92e10d7f8e1','t' );
 
+declare @iduser int;
+select @iduser =  id from Users where Login ='toko';
+
+begin
 insert into Proposition (Id_user, UpdateDate,Status)
-values (4,CONVERT(VARCHAR(19),GETDATE()),'New');
-insert into PropAccomodation (Id_proposition,TypeOfRoom,BruttoPrice,Vat,Amount,Days)
-select p.Id,'DoubleRoomEP',450,23,3,10  from Proposition p  where p.Id_user = 4;
-insert into PropAccomodationDiscount ( Id_proposition, StandardPrice,Discount,DoubleRoomEP,BussinesSingleEP,ApartmentSingleEP)
-select p.Id,100,20,45,100,220 from Proposition p where p.Id_user = 4;
+values (@iduser,CONVERT(VARCHAR(19),GETDATE()),'Nowa');
 insert into PropClient (Id_proposition,CompanyName,CompanyAdress,NIP,CustomerFullName,PhoneNum,DecisingPersonFullName, CustomerEmail)
-select  p.Id,'Wytwórnia win','Jagodowa 3 23-765 Siemianowiec','1234556698','Sebastian Nalepka', 500295458,'Patrycja Kowalska' ,'sn@wp.pl'   from Proposition p;
-insert into PropExtraServices(Id_proposition,ServiceType,BruttoPrice,Vat,Days)
-select  p.Id,'Inne ',10,8,3  from Proposition p where p.Id_user = 4;
-insert into PropExtraServicesDiscount(Id_proposition,StandardPrice,Discount)
-select  p.Id,100,50  from Proposition p where p.Id_user = 4;
-insert into PropHallEquipment(Id_proposition,Things,BruttoPrice,Vat,Amount,Days)
-select  p.Id,'RZUTNIK',10,8,2,10  from Proposition p where p.Id_user = 4;
-insert into PropHallEquipment(Id_proposition,Things,BruttoPrice,Vat,Amount,Days)
-select  p.Id,'LAPTOP',110,8,2,10  from Proposition p where p.Id_user = 4;
-insert into PropHallEquipmentDiscount(Id_proposition,StandardPrice,Discount) 
-select p.Id,45,20 from Proposition p where p.Id_user = 4 ;
-insert into PropMenuMerge(Id_proposition,MergeName,DefaultValue,MergeType)
-select  p.Id,'Marża gastronomia 8%',80,'MG8' from Proposition p where p.Id_user = 4;
-
-
-insert into PropMenuMerge(Id_proposition,MergeName,DefaultValue,MergeType)
-select p.Id,'Marża gastronomia 23%',50,'MG23'  from Proposition p where p.Id_user = 4;
-insert into PropMenuMerge(Id_proposition,MergeName,DefaultValue,MergeType)
-select  p.Id,'Marża alkohole',30,'MA'  from Proposition p where p.Id_user = 4;
-insert into PropMenuMerge(Id_proposition,MergeName,DefaultValue,MergeType)
-select  p.Id,'Marża napoje',20,'MN'  from Proposition p where p.Id_user = 4;
-
-
-insert into PropMenuPosition(Id_proposition,TypeOfService,BruttoPrice,Vat,Amount,Days)
-select  p.Id,'Lunch serwowany - 3 dania',80,8,3,4  from Proposition p where p.Id_user = 4;
-insert into PropMenuPosition(Id_proposition,TypeOfService,BruttoPrice,Vat,Amount,Days) 
-select  p.Id,'Lunch serwowany - Menu I Sląskie smaki',50,8,3,4  from Proposition p where p.Id_user = 4;
-insert into PropMenuPosition(Id_proposition,TypeOfService,BruttoPrice,Vat,Amount,Days)
-select  p.Id,'Wyborowa0,5 l',40,23,3,4 from Proposition p where p.Id_user = 4;
-insert into PropPaymentSuggestions (Id_proposition,PaymentForm,IndividualOrders,InvoiceServiceName,CarPark,HallDescription)
-select   p.Id,'Gotówka/karta','Doliczany do faktury','Posiłki grupowe HP','Brak','teatral' from Proposition p where p.Id_user = 4;
+select  p.Id,'Wytwórnia win','Jagodowa 3 23-765 Siemianowiec','1234556698','Łukasz Lis', 500295458,'Łukasz Lis' ,'sn@wp.pl'   from Proposition p;
 insert into PropReservationDetails (Id_proposition,StartData,StartTime,EndData,EndTime,PeopleNumber,Hall,HallSetting)
-select  p.Id,CONVERT(VARCHAR(19),GETDATE()), '10:15:00' ,CONVERT(VARCHAR(19),(GETDATE()+100)), '14:15:00',6,'C','Teatra' from Proposition p;
+select  p.Id,CONVERT(VARCHAR(19),2015-12-29), '10:15:00' ,CONVERT(VARCHAR(19),2015-12-29), '14:15:00',6,'A',NULL from Proposition p;
+insert into PropHallEquipment(Id_proposition,Things,BruttoPrice,Vat,Amount,Days)
+select  p.Id,'Sala A',285,23,3,4  from Proposition p where p.Id_user = @iduser;
+insert into PropHallEquipmentDiscount(Id_proposition,StandardPrice,Discount) 
+select p.Id,300,5 from Proposition p where p.Id_user = @iduser ;
+insert into PropMenuPosition(Id_proposition,TypeOfService,BruttoPrice,Vat,Amount,Days)
+select  p.Id,'Lunch serwowany - 2 dania z przystawką',63.1401443,8,2,3  from Proposition p where p.Id_user = @iduser;
+
+insert into PropMenuMerge(Id_proposition,MergeName,DefaultValue,MergeType)
+select  p.Id,'Marża gastronomia 8%)',77,'MGA8' from Proposition p where p.Id_user = @iduser;
+insert into PropMenuMerge(Id_proposition,MergeName,DefaultValue,MergeType)
+select p.Id,'MMarża gastronomia 23%)',78,'MGA23'  from Proposition p where p.Id_user = @iduser;
+insert into PropMenuMerge(Id_proposition,MergeName,DefaultValue,MergeType)
+select  p.Id,'Marża Alkohole)',78,'MALK'  from Proposition p where p.Id_user = @iduser;
+insert into PropMenuMerge(Id_proposition,MergeName,DefaultValue,MergeType)
+select  p.Id,'Marża niskobudżetowe)',58,'MNIS'  from Proposition p where p.Id_user = @iduser;
+insert into PropMenuMerge(Id_proposition,MergeName,DefaultValue,MergeType)
+select  p.Id,'Napoje)',48,'MNAPO'  from Proposition p where p.Id_user = @iduser;
+
+insert into PropAccomodation (Id_proposition,TypeOfRoom,BruttoPrice,Vat,Amount,Days)
+select p.Id,'POKÓJ 1-OSOBOWY',242.5,8,3,3  from Proposition p  where p.Id_user = @iduser;
+insert into PropAccomodationDiscount ( Id_proposition, StandardPrice,Discount,DoubleRoomEP,BussinesSingleEP,ApartmentSingleEP)
+select p.Id,NULL,3,NULL,NULL,NULL from Proposition p where p.Id_user = @iduser;
+insert into PropExtraServices(Id_proposition,ServiceType,BruttoPrice,Vat,Days,Amount)
+select  p.Id,'PARKING ( część dozorowana )',3.5,8,3,3  from Proposition p where p.Id_user = @iduser;
+insert into PropExtraServices(Id_proposition,ServiceType,BruttoPrice,Vat,Days,Amount)
+select  p.Id,'Inne',5,8,3,3  from Proposition p where p.Id_user = @iduser;
+insert into PropPaymentSuggestions (Id_proposition,PaymentForm,IndividualOrders,InvoiceServiceName,CarPark,HallDescription)
+select   p.Id,'100% do dnia realizacji','Doliczany do faktury','Gastronomia konferencyjna 8% ; Gastronomia konferencyjna 23% ; Wynajem Sali 23% ; Usługa noclegowa 8%','Płatny indywidualnie',null from Proposition p where p.Id_user = @iduser;
+end
+
+
+
+
+
+
 
 insert into Proposition (Id_user, UpdateDate,Status)
 values (6,CONVERT(VARCHAR(19),GETDATE()+100),'New');
